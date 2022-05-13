ad22374d2e72e774d69ef950122146f5d800988d
B1ELL20 <dantasbiell20@gmail.com>
2022-05-12 23:19:09 -0300

    responsivo pagina login

--- sucesso.php
+++ sucesso.php
@@ -17,6 +17,8 @@
 
     <script src="script.js"></script>
 
+    <link rel="shortcut icon" href="icons8-usuário-30.png" type="image/x-icon">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <style>
@@ -37,7 +39,7 @@
             <h1>Seja bem-vindo</h1>
             <h3><?= $_GET['login'] ?></h3>
             <p class="mt-3">Este programa foi desenvolvido com Html, CSS com Bootstrap, JavaScript com JQuery e um banco de dados MySQL para o registro de cadastros!</p>
-            <a href="index.html"><button class="btn btn-outline-danger btn-lg mt-3">Voltar para tela de login</button></a>
+            <a href="index.php"><button class="btn btn-outline-danger btn-lg mt-3">Voltar para tela de login</button></a>
         </div>
     </div>
 </body>
